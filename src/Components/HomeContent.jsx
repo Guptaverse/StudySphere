f81d939f699169a8b72d1e3e320c1483b2ccc43a
Ahzam Syed <ahzam183@gmail.com>
2023-04-27 19:38:46 +0530

    updated

--- src/Components/HomeContent.jsx
+++ src/Components/HomeContent.jsx
@@ -8,21 +8,9 @@ const HomeContent = () => {
       <div className='topSection'>
         <div className='logo'>
           <div className='sphere'></div>
-          <h3>
-            <span className='s'>S</span>
-            <span className='t'>t</span>
-            <span className='u'>u</span>
-            <span className='d'>d</span>
-            <span className='e'>e</span>
-            <span className='n'>n</span>
-            <span className='t1'>t</span>
-            <span className='s1'>S</span>
-            <span className='p'>p</span>
-            <span className='h'>h</span>
-            <span className='e1'>e</span>
-            <span className='r'>r</span>
-            <span className='s2'>e</span>
-          </h3>
+          <h2>
+            StudentSphere
+          </h2>
         </div>
         <Searchbar />
       </div>
